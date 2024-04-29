6316158ad985f9c5e7bfec7026957063667c414f
Clemence Fortunato <c.fortunato@agence-wam.fr>
2024-04-29 15:08:45 +0200

    Add absolute imports + fix sass cmd error

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -1,7 +1,10 @@
+import Button from 'frontEnd/components/Button';
+
 export default function Home() {
   return (
     <main>
       <h1>projet cascabelle</h1>
+      <Button />
     </main>
   );
 }
