1d86b35b08b34b010790a2b3887cb5f2b8a97046
Clemence Fortunato <c.fortunato@agence-wam.fr>
2024-05-10 10:08:23 +0200

    Seed type, genre, support, role, subscription and event_type  tables in database

---
+++ src/backEnd/database/seedData.ts
@@ -0,0 +1,125 @@
+export const types = [
+  {
+    label: 'livre',
+  },
+  {
+    label: 'cd',
+  },
+  {
+    label: 'dvd',
+  },
+  {
+    label: 'périodique',
+  },
+];
+
+export const genres = [
+  {
+    label: 'polar',
+  },
+  {
+    label: 'fantastique',
+  },
+  {
+    label: 'science-fiction',
+  },
+  {
+    label: 'non-fiction',
+  },
+  {
+    label: 'romance',
+  },
+  {
+    label: 'rock n\' roll',
+  },
+  {
+    label: 'pop',
+  },
+  {
+    label: 'country',
+  },
+  {
+    label: 'classique',
+  },
+  {
+    label: 'rap',
+  },
+  {
+    label: 'reggae',
+  },
+  {
+    label: 'variété française',
+  },
+  {
+    label: 'quotidien',
+  },
+  {
+    label: 'jardinage',
+  },
+  {
+    label: 'économie',
+  },
+  {
+    label: 'mode et beauté',
+  },
+];
+
+export const supports = [
+  {
+    label: 'physique',
+  },
+  {
+    label: 'disque',
+  },
+  {
+    label: 'digital',
+  },
+  {
+    label: 'audiobook',
+  },
+];
+
+export const roles = [
+  {
+    label: 'abonné',
+  },
+  {
+    label: 'bibliothécaire',
+  },
+  {
+    label: 'admin',
+  },
+];
+
+export const subscriptions = [
+  {
+    name: 'enfants',
+    monthly_price: 1,
+    annual_price: 10,
+  },
+  {
+    name: 'biblio',
+    monthly_price: 2,
+    annual_price: 18,
+  },
+  {
+    name: 'culture',
+    monthly_price: 4,
+    annual_price: 38,
+  },
+];
+
+export const eventTypes = [
+  {
+    label: 'musique',
+  },
+  {
+    label: 'rencontre artiste',
+  },
+  {
+    label: 'groupe de lecture',
+  },
+  {
+    label: 'exposition',
+  },
+];
---
+++ src/backEnd/database/seedTables.ts
@@ -0,0 +1,163 @@
+/* eslint-disable no-console */
+import { VercelPoolClient, db } from '@vercel/postgres';
+
+import {
+  types,
+  genres,
+  roles,
+  eventTypes,
+  supports,
+  subscriptions,
+} from './seedData';
+
+async function seedTypeTable(client: VercelPoolClient) {
+  try {
+    const insertedTypes = await Promise.all(
+      types.map(
+        (type) => client.sql`
+        INSERT INTO type (label)
+        VALUES (${type.label})
+      `,
+      ),
+    );
+
+    console.log(`Seeded ${insertedTypes.length} Types`);
+
+    return {
+      types: insertedTypes,
+    };
+  } catch (error) {
+    console.error('Error seeding type:', error);
+    throw error;
+  }
+}
+
+async function seedGenreTable(client: VercelPoolClient) {
+  try {
+    const insertedGenres = await Promise.all(
+      genres.map(
+        (genre) => client.sql`
+        INSERT INTO genre (label)
+        VALUES (${genre.label})
+      `,
+      ),
+    );
+
+    console.log(`Seeded ${insertedGenres.length} genres`);
+
+    return {
+      genres: insertedGenres,
+    };
+  } catch (error) {
+    console.error('Error seeding genre:', error);
+    throw error;
+  }
+}
+
+async function seedSupportTable(client: VercelPoolClient) {
+  try {
+    const insertedSupports = await Promise.all(
+      supports.map(
+        (support) => client.sql`
+        INSERT INTO support (label)
+        VALUES (${support.label})
+      `,
+      ),
+    );
+
+    console.log(`Seeded ${insertedSupports.length} supports`);
+
+    return {
+      supports: insertedSupports,
+    };
+  } catch (error) {
+    console.error('Error seeding support:', error);
+    throw error;
+  }
+}
+
+async function seedRoleTable(client: VercelPoolClient) {
+  try {
+    const insertedRoles = await Promise.all(
+      roles.map(
+        (role) => client.sql`
+        INSERT INTO role (label)
+        VALUES (${role.label})
+      `,
+      ),
+    );
+
+    console.log(`Seeded ${insertedRoles.length} Types`);
+
+    return {
+      roles: insertedRoles,
+    };
+  } catch (error) {
+    console.error('Error seeding role:', error);
+    throw error;
+  }
+}
+
+async function seedSubscriptionTable(client: VercelPoolClient) {
+  try {
+    const insertedSubscriptions = await Promise.all(
+      subscriptions.map(
+        (subscription) => client.sql`
+        INSERT INTO subscription (name, monthly_price, annual_price)
+        VALUES (${subscription.name},${subscription.monthly_price},${subscription.annual_price})
+      `,
+      ),
+    );
+
+    console.log(`Seeded ${insertedSubscriptions.length} subscriptions`);
+
+    return {
+      subscriptions: insertedSubscriptions,
+    };
+  } catch (error) {
+    console.error('Error seeding subscription:', error);
+    throw error;
+  }
+}
+
+async function seedEventTypeTable(client: VercelPoolClient) {
+  try {
+    const insertedEventTypes = await Promise.all(
+      eventTypes.map(
+        (eventType) => client.sql`
+        INSERT INTO event_type (label)
+        VALUES (${eventType.label})
+      `,
+      ),
+    );
+
+    console.log(`Seeded ${insertedEventTypes.length} Types`);
+
+    return {
+      eventTypes: insertedEventTypes,
+    };
+  } catch (error) {
+    console.error('Error seeding eventType:', error);
+    throw error;
+  }
+}
+
+async function main() {
+  try {
+    const client = await db.connect();
+
+    await seedTypeTable(client);
+    await seedGenreTable(client);
+    await seedRoleTable(client);
+    await seedSupportTable(client);
+    await seedSubscriptionTable(client);
+    await seedEventTypeTable(client);
+  } catch (error) {
+    console.error(
+      'An error occurred while attempting to seed the database:',
+      error,
+    );
+  }
+}
+
+export default main;
